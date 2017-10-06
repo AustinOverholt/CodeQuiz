cc2bb6808835b0d9ffa79b5ae0dafc28e63d69c2
Austin Overholt <austinoverholt@gmail.com>
2017-10-06 12:57:36 -0700

    Added angular quiz controller and added to layout

---
+++ CodeQuiz/Scripts/quizController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module("mainApp")
+        .controller("quizController", quizController);
+
+    quizController.$inject = ["$scope"]; // put service here when created
+
+    function quizController($scope) {
+
+        var vm = this;
+        vm.$scope = $scope;
+        vm.$onInit = _init;
+
+        function _init() {
+            console.log("quiz controller initialized");
+        }
+    }
+
+
+
+
+
+
+
+})();
