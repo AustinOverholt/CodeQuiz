8b53828cf54dd9baa33c82095804d2212e0802bd
Austin Overholt <austinoverholt@gmail.com>
2017-10-08 15:52:51 -0700

    Fixed test conn string Added unit test for select all Test Passes!

---
+++ CodeQuiz.Tests/Services/QuizTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeQuiz.Services;
+using CodeQuiz.Model.Domain;
+using System.Collections.Generic;
+
+namespace CodeQuiz.Tests
+{
+    [TestClass]
+    public class QuizTest
+    {
+        [TestMethod]
+        public void SelectQuizTest()
+        {
+            QuizService svc = new QuizService();
+            List<Quiz> model = svc.SelectAll();
+            Assert.IsNotNull(model);
+        }
+    }
+}
