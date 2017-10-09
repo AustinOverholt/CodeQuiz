6c88c5ce6e15479bb17017185e708cbd2508893c
Austin Overholt <austinoverholt@gmail.com>
2017-10-08 17:54:19 -0700

    Added delete stored proc for quiz

---
+++ CodeQuiz.Database/Quiz_Delete.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[Quiz_Delete]
+@Id int
+/*
+Select *
+From dbo.Quiz
+
+Declare @_id int = 4
+
+Execute dbo.Quiz_Delete
+	@_id
+
+Select *
+From dbo.Quiz
+*/
+AS
+BEGIN
+DELETE FROM [dbo].[Quiz]
+      WHERE Id = @Id
+END
