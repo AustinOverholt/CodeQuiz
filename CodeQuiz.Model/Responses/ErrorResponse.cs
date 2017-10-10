642c6e14d374395c8c7d767758b52ae7f15c4d20
Austin Overholt <austinoverholt@gmail.com>
2017-10-10 13:54:38 -0700

    Added responses for Api

---
+++ CodeQuiz.Model/Responses/ErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeQuiz.Model.Responses
+{
+    public class ErrorResponse : BaseResponse
+    {
+        public ErrorResponse()
+        {
+            this.IsSuccessful = false;
+        }
+    }
+}
