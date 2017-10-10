08dc35e5c2300013a1288365495558bfca82835d
Austin Overholt <austinoverholt@gmail.com>
2017-10-09 22:20:31 -0700

    Cleaned up and added required

--- CodeQuiz.Model/Requests/QuizUpdateRequest.cs
+++ CodeQuiz.Model/Requests/QuizUpdateRequest.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CodeQuiz.Model.Requests
 {
     public class QuizUpdateRequest : QuizAddRequest
     {
+        [Required]
         public int Id { get; set; }
     }
 }
