a655f352593c9bfc26eefa5eef05b2b196ce6bbc
Austin Overholt <austinoverholt@gmail.com>
2017-10-10 14:03:25 -0700

    Added quiz interface

---
+++ CodeQuiz.Services/Interfaces/IQuizService.cs
@@ -0,0 +1,18 @@
+﻿using CodeQuiz.Model.Domain;
+using CodeQuiz.Model.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeQuiz.Services.Interfaces
+{
+    public interface IQuizService
+    {
+        List<Quiz> SelectAll();
+        int Insert(QuizAddRequest model);
+        void Update(QuizUpdateRequest model);
+        void Delete(int id);
+    }
+}
