ef34cea5247005cb1834b168f4c34c2b50ec1663
Austin Overholt <austinoverholt@gmail.com>
2017-10-10 15:01:19 -0700

    Get working in Api Created constructor for service Might not have been working because of wrong reference Go back and fix later

--- CodeQuiz/Controllers/Api/QuizApiController.cs
+++ CodeQuiz/Controllers/Api/QuizApiController.cs
@@ -1,50 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Web.Http;
-using CodeQuiz.Model.Domain;
-using CodeQuiz.Model.Requests;
-using System.Net.Http;
+﻿using CodeQuiz.Model.Domain;
+using CodeQuiz.Model.Responses;
+using CodeQuiz.Services;
 using System;
 using System.Net;
-using CodeQuiz.Model.Responses;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace CodeQuiz.Controllers.Api
 {
     [RoutePrefix("api/quiz")]
     public class QuizApiController : ApiController
     {
-        private IQuizService _quizService;
-        
-        public QuizApiController(IQuizService QuizService)
-        {
-            _quizService = QuizService;
-        }
+        QuizService quizService = new QuizService();
+
         [Route(), HttpGet]
         public HttpResponseMessage Get()
         {
             try
             {
                 ItemsResponse<Quiz> resp = new ItemsResponse<Quiz>();
-                resp.Items = _quizService.SelectAll();
+                resp.Items = quizService.SelectAll();
                 return Request.CreateResponse(HttpStatusCode.OK, resp);
             }
             catch (Exception ex)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
-        }
-        public void Delete(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int Insert(QuizAddRequest model)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Update(QuizUpdateRequest model)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
