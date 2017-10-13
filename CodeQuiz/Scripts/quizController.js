6050b57378e25e071ab0266ae0a1e4f8b0f731a0
Austin Overholt <austinoverholt@gmail.com>
2017-10-12 17:40:48 -0700

    Added ng click for new button edit button delete button need to add functionality

--- CodeQuiz/Scripts/quizController.js
+++ CodeQuiz/Scripts/quizController.js
@@ -13,6 +13,9 @@
         vm.$onInit = _init;
         vm.mainService = mainService;
         vm.quizList = {};
+        vm.newButton = _newButton;
+        vm.editButton = _editButton;
+        vm.deleteButton = _deleteButton;
 
         function _init() {
             console.log("quiz controller initialized");
@@ -33,6 +36,18 @@
             function _getFailed(err) {
                 console.log("Get Failed", err);
             }
+        }
+
+        function _newButton() {
+            console.log("new button clicked");
+        }
+
+        function _editButton() {
+            console.log("edit button clicked");
+        }
+
+        function _deleteButton() {
+            console.log("delete button clicked");
         }
     }
 })();
