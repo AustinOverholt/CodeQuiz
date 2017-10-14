b558c585502e64b529d454a0c5def3ed4b384a2c
Austin Overholt <austinoverholt@gmail.com>
2017-10-14 14:51:31 -0700

    Created Select By Id stored proc

---
+++ CodeQuiz.Database/Quiz_SelectById.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[Quiz_SelectById]
+	@Id int
+/*
+Declare @_id int = 1
+Execute dbo.Quiz_SelectById 
+	@_id 
+*/
+AS
+BEGIN
+SELECT [Id]
+      ,[Category]
+      ,[Question]
+      ,[Answer1]
+      ,[Answer2]
+      ,[Answer3]
+      ,[Answer4]
+  FROM [dbo].[Quiz]
+  WHERE Id = @Id
+END
+
