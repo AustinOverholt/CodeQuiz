49f9d8c896a53024a7e635b22acbfe9e8858933e
Austin Overholt <austinoverholt@gmail.com>
2017-10-16 17:08:19 -0700

    Created new controller for quiz taking page Seperated scripts

---
+++ CodeQuiz/Scripts/takeQuizController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+    angular 
+        .module("mainApp")
+        .controller("takeQuizController", takeQuizController)
+
+    takeQuizController.$inject = ["$scope", "mainService", "toastr"];
+
+    function takeQuizController($scope, mainService, toastr) {
+        var vm = this;
+        vm.$scope = $scope;
+        vm.$onInit = _init;
+
+        function _init() {
+            toastr.success("Initialization successful");
+        }
+    }
+
+
+})();
