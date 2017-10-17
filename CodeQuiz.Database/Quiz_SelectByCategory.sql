580b2578590cc706a2fcbac6ac5dd800893787cc
Austin Overholt <austinoverholt@gmail.com>
2017-10-16 17:35:00 -0700

    Created stored procedure for selecting by category

---
+++ CodeQuiz.Database/Quiz_SelectByCategory.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROC [dbo].[Quiz_SelectByCategory]
+	@Category nvarchar(128)
+/*
+Declare @_category nvarchar(128)  = 'C#'
+Execute dbo.Quiz_SelectByCategory 
+	@_category
+*/
+AS
+BEGIN
+SELECT [Id]
+      ,[Category]
+      ,[Question]
+      ,[Answer1]
+      ,[Answer2]
+      ,[Answer3]
+      ,[Answer4]
+  FROM [dbo].[Quiz]
+  WHERE Category = @Category
+END
