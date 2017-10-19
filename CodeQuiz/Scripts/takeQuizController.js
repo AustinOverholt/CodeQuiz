a5d71c74b9813cacf1a5aafccf19c699770ba20a
Austin Overholt <austinoverholt@gmail.com>
2017-10-19 13:59:42 -0700

    Got controller working and quiz questions showing Just an example of only the first question showing

--- CodeQuiz/Scripts/takeQuizController.js
+++ CodeQuiz/Scripts/takeQuizController.js
@@ -11,16 +11,30 @@
         vm.$scope = $scope;
         vm.$onInit = _init;
         vm.quizType;
+        vm.quizList = {};
         vm.quizChoose = _quizChoose;
 
         function _init() {
             // initialization here
+            _quizChoose(); // get rid of this when basing quiz on category
         }
 
         // Get for category type
         function _quizChoose() {
             toastr.success(vm.quizType);
             // Filter based on chosen category
+            mainService.get("/api/quiz/") // Get quiz based on category for now just getting all quizzes
+                .then(_getQuizSuccess)
+                .catch(_getQuizFailed)
+
+            function _getQuizSuccess(res) {
+                vm.quizList = res.data.Items;
+                console.log(vm.quizList);
+            }
+
+            function _getQuizFailed(err) {
+                toastr.error("Get Failed");
+            }
         }
     }
 
