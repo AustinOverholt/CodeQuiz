147bbdc17e5a5a87b670282626655a525c4fc7ab
Austin Overholt <austinoverholt@gmail.com>
2017-10-25 20:36:52 -0700

    Added interface for quiz service

---
+++ CodeQuiz.Services/Interfaces/IQuizService.cs
@@ -0,0 +1,18 @@
+﻿using CodeQuiz.Model.Domain;
+using CodeQuiz.Model.Requests;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CodeQuiz.Services.Interfaces
+{
+    public interface IQuizService
+    {
+        List<Quiz> SelectAll();
+        List<Quiz> SelectByCategory(string Category);
+        Quiz SelectById(int Id);
+        int Insert(QuizAddRequest model);
+        void Update(QuizUpdateRequest model);
+        void Delete(int id);
+        Quiz Mapper(SqlDataReader reader);
+    }
+}
