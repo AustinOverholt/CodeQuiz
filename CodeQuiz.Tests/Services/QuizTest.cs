384e721c2948dd6c5b20231327df363f72f5303a
Austin Overholt <austinoverholt@gmail.com>
2017-12-07 21:09:10 -0800

    Created a unit test for insert. Works!

--- CodeQuiz.Tests/Services/QuizTest.cs
+++ CodeQuiz.Tests/Services/QuizTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CodeQuiz.Services;
 using CodeQuiz.Model.Domain;
 using System.Collections.Generic;
+using CodeQuiz.Model.Requests;
 
 namespace CodeQuiz.Tests
 {
@@ -29,7 +30,27 @@ namespace CodeQuiz.Tests
         [TestMethod] 
         public void InsertQuizTest()
         {
+            //string Category = "C#";
+            //string Question = "?";
+            //string Answer1 = "Idk";
+            //string Answer2 = "Idk";
+            //string Answer3 = "Idk";
+            //string Answer4 = "IK";
+            //string Correct = "4";
 
+            QuizAddRequest model = new QuizAddRequest();
+
+            model.Category = "C#";
+            model.Question = "?";
+            model.Answer1 = "IDK";
+            model.Answer2 = "IDK";
+            model.Answer3 = "IDK";
+            model.Answer4 = "IK";
+            model.Correct = 4;
+
+            svc.Insert(model);
+
+            Assert.IsNotNull(model);
         }
 
         [TestMethod]
